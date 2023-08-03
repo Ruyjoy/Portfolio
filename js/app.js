4aae3986cd947b86ae4e729fbbbba34462c551eb
José Luis <Ruyjoy@hotmail.com>
2023-08-03 15:24:15 -0300

    Cambios cpmpletos en body

--- js/app.js
+++ js/app.js
@@ -19,7 +19,6 @@ fetch('./json/datos.json')
         console.log(error);
     });
 
-
     const cargarCards= data =>{
 
         data.forEach(porducto =>{
@@ -32,4 +31,36 @@ fetch('./json/datos.json')
         items.appendChild(fragment);
     }
 
+
+    const divcard = document.getElementById("card-imagen");
+    const templatecard = document.getElementById("card-portfolio").content;
+    const fragment2 = document.createDocumentFragment();
+    
+    fetch('./json/cardData.json')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (res) {
+    
+           cargarPortfolio(res)
+            
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+        
+        const cargarPortfolio= res =>{
+    
+            res.forEach(i =>{
+                templatecard.querySelector('#imagen-conta').setAttribute("title",i.titulo)
+                templatecard.querySelector('img').setAttribute("src",i.url)
+                templatecard.querySelector('h5').textContent = i.titulo;
+                templatecard.querySelector('p').textContent = i.description;
+                
+                const clone1 = templatecard.cloneNode(true);
+                fragment2.appendChild(clone1);
+            })
+    
+            divcard.appendChild(fragment2);;
+        }
    
