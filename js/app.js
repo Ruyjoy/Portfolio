00430c7d79daa03043797a9c1cd219454c498c06
José Luis <Ruyjoy@hotmail.com>
2023-08-30 22:23:54 -0300

    Ahora

--- js/app.js
+++ js/app.js
@@ -5,9 +5,9 @@ $(function(){
     //$("#portfolio").load("./template/portfolio.html");
     //$("#skillset").load("./template/skills.html");
     //$("#contact").load("./template/contact.html");
-    $("#proyectos").load("./template/proyectos.html");
+    //$("#proyectos").load("./template/proyectos.html");
     $("#about").load("./template/sobreMi.html");
-    $("#skillset").load("./template/skill.html");
+    //$("#skillset").load("./template/skill.html");
     
 });
 
@@ -20,7 +20,37 @@ $(function(){
 
 
 
-
+    const items = document.getElementById("items");
+    const template = document.getElementById("template-card").content;
+    const fragment = document.createDocumentFragment();
+    
+    fetch('./json/datos.json')
+        .then(function (response) {
+            return response.json();
+        })
+        .then( (data) => {
+    
+           
+           cargarCards(data);
+    
+            
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    
+        const cargarCards= (data) =>{
+    
+            data.forEach(porducto =>{
+                template.querySelector('img').setAttribute("title",porducto.name)
+                template.querySelector('img').setAttribute("src",porducto.url)
+                const clone = template.cloneNode(true);
+                fragment.appendChild(clone);
+            })
+    
+            items.appendChild(fragment);
+        }
+    
 
 
 
